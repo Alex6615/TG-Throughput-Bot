9296be92f1ecf5bb6f3f0e85fa1f9514051a6c66
alex.li <alex.li@owin.corp>
2023-06-19 17:35:59 +0800

    test

--- oclock.py
+++ oclock.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def isoclock() -> bool: 
     nows = int(time.time())
     dt = datetime.fromtimestamp(nows)
-    if dt.minute == 0 or dt.minute == 30 :
+    if dt.minute == 0 or dt.minute == 10 or dt.minute == 20 or dt.minute == 30 or dt.minute == 40 or dt.minute == 50 :
         return True
     else :
         return False
