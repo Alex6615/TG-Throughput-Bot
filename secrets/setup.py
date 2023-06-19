54d5ee166f06c23408f2f19e619f501793ea77f6
alex.li <alex.li@owin.corp>
2023-06-19 15:11:25 +0800

    fix setup path

--- secrets/setup.py
+++ secrets/setup.py
@@ -3,5 +3,5 @@ cwd = os.getcwd()
 from distutils.core import setup
 from Cython.Build import cythonize
  
-setup(ext_modules=cythonize([f"{cwd}/bak/secret_telegram.py"]))
-setup(ext_modules=cythonize([f"{cwd}/bak/secret_account.py"]))
+setup(ext_modules=cythonize([f"{cwd}/secrets/secret_telegram.py"]))
+setup(ext_modules=cythonize([f"{cwd}/secrets/secret_account.py"]))
