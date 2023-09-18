badd59a8860c0c57d6e5dc74560b1ec1b2765a8f
alex.li <alex.li@owin.corp>
2023-09-18 09:16:36 +0800

    add grafana waiting time

--- driver_execute.py
+++ driver_execute.py
@@ -29,7 +29,7 @@ def get_Image(now, before_hours):
     # submit
     password_input = driver.find_element(By.XPATH, "//*[@id='reactRoot']/div[1]/main/div/div[3]/div/div[2]/div/div/form/button/span")
     password_input.click()
-    time.sleep(1)
+    time.sleep(2)
     driver.get(f"http://{grafana}/render/d-solo/93LKRJP4z/es-goedge-log-traffic?orgId=1&refresh=30s&from={before_hours}&to={now}&panelId=69&width=1000&height=500&tz=Asia%2FTaipei")
     driver.get_screenshot_as_file(f"{pwd}/png/throughput-{now}.png")
     driver.quit()
