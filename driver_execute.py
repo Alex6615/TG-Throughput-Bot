59a90d50305d43805351f8e0abcae542b86b92b0
alex.li <alex.li@owin.corp>
2023-09-25 09:27:43 +0800

    fix chrome setting

--- driver_execute.py
+++ driver_execute.py
@@ -18,7 +18,8 @@ from time_generator import ts_generator
 
 def get_Image(now, before_hours):
     pwd = os.getcwd()
-    driver = get_ChromeDriver(os="mac", headless=False)
+    #driver = get_ChromeDriver(os="mac", headless=False)
+    driver = get_ChromeDriver()
     driver.get(f"http://{grafana}/login")
     locator_loginpage = (By.XPATH, '//*[@id="pageContent"]/div/div/div[3]/div/div/div[1]/img')
     try :
